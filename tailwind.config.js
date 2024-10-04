5ac199a0c0f9641d20bc73a7b58af08b81b66078
Vishal <vishal.bhumca21@gmail.com>
2024-10-04 16:55:00 +0530

    My First Tailwind Project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    
+  },
+  plugins: [
+    // ...
+    require('tailwind-scrollbar'),
+],
+}
+
